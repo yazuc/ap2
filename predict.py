0eaeff9b3064ad643ca3e46072f06153d40cf7dd
yazuc <51753599+yazuc@users.noreply.github.com>
2023-11-19 18:24:42 -0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -12,17 +12,8 @@ img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
 img_array /= 255.0  # Normalize the pixel values to be between 0 and 1
 
-# Load and preprocess the image you want to predict
-img_path_cat = 'C:/Users/Leonardo/Downloads/ap2/cat.png'
-img_cat = image.load_img(img_path_cat, target_size=(150, 150))
-img_array_cat = image.img_to_array(img_cat)
-img_array_cat = np.expand_dims(img_array_cat, axis=0)
-img_array_cat /= 255.0  # Normalize the pixel values to be between 0 and 1
-
 # Make predictions
 predictions = model.predict(img_array)
-
-predictions_cat = model.predict(img_array_cat)
 
 # The model.predict() function returns the probability of the image belonging to class 1 (dog).
 # You can interpret the result based on your binary classification task.
@@ -32,7 +23,3 @@ if predictions[0][0] > 0.5:
 else:
     print("It's a cat!")
 
-if predictions_cat[0][0] > 0.5:
-    print("It's a dog!")
-else:
-    print("It's a cat!")    
