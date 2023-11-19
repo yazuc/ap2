620d7f2e7295da834db1300e048aef01b337a7e4
yazuc <51753599+yazuc@users.noreply.github.com>
2023-11-19 18:44:26 -0300

    melhoria readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ap2
  trab ap2
+
+é necessário alterar os locais conforme sua máquina
+ex de variaveis a alterar 
+    ap.py: train_directory
+    predict.py: img_path
+    reorder.py: dataset_path
+
+compilação dos arquivos com python3
+
+python3 ap.py
+
+python3 predict.py
+
+python3 reorder.py basicamente monta o diretorio de treino da maneira que ap.py consegue entender
--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import load_model
 model = load_model('my_model.h5')  # Replace with the path to your saved model
 
 # Load and preprocess the image you want to predict
-img_path = 'C:/Users/Leonardo/Downloads/ap2/dog.jpg'
+img_path = 'C:/Users/Leonardo/Downloads/ap2/stella.jpg'
 img = image.load_img(img_path, target_size=(150, 150))
 img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0)
